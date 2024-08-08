cf1e1b20b93ed57f15cad1edc88453f6b242c150
AsmaeElati <148854960+AsmaeElati@users.noreply.github.com>
2024-08-08 22:59:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,5 @@
 import streamlit as st
 import pandas as pd
-
-# Function to load data
-@st.cache
-def load_data(file_path):
-    data = pd.read_csv(file_path, sep=';')
-    return data
-
 # Streamlit app layout
 st.title('CSV Data Viewer')
 
@@ -14,10 +7,8 @@ st.write('This app loads and displays data from a CSV file.')
 
 # Path to the local CSV file
 file_path = 'Final_Avito_Dataset.csv'  # Update with your local file path
+data_cleaned = pd.read_csv(file_path, sep=';')
 
-if file_path:
-    try:
-        data_cleaned = load_data(file_path)
-        st.write(data_cleaned.head())
-    except Exception as e:
-        st.error(f"Error loading data: {e}")
+# Afficher un aperçu des données
+data_cleaned.head()
+
