2cab9dc542c6aa42756d655ac89e968123425bb5
AsmaeElati <148854960+AsmaeElati@users.noreply.github.com>
2024-08-08 22:37:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,25 @@ import streamlit as st
 st.title('🎈 App Name')
 
 st.info('building my model')
+import streamlit as st
+import pandas as pd
+
+# Function to load data
+@st.cache
+def load_data(file_path):
+    data = pd.read_csv(file_path, sep=';')
+    return data
+
+# Streamlit app layout
+st.title('CSV Data Viewer')
+
+st.write('This app loads and displays data from a CSV file on Google Drive.')
+
+file_path = st.text_input('Enter the file path:', '/content/drive/My Drive/Avito_Dataset old.csv')
+
+if file_path:
+    try:
+        data_cleaned = load_data(file_path)
+        st.write(data_cleaned.head())
+    except Exception as e:
+        st.error(f"Error loading data: {e}")
